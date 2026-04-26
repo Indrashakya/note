73fcf1c966aa256e338b375ac7d8a7f6eb4df297
Indrashakya <107881309+Indrashakya@users.noreply.github.com>
2026-04-25 21:01:17 -0400

    Improve notes app UX with Apple Notes-inspired features

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,21 @@
-import * as React from 'react';
-import { Container, Navbar } from 'react-bootstrap';
+import * as React from "react";
+import { Container, Navbar } from "react-bootstrap";
 
-interface IHeaderProps {
+interface HeaderProps {
+  totalNotes: number;
+  pinnedNotes: number;
 }
 
-const Header: React.FunctionComponent<IHeaderProps> = (props) => {
-    return (
-        <Navbar fixed="top" bg="dark" variant="dark">
-            <Container>
-                <Navbar.Brand>
-                    This is Demo noteapp
-                </Navbar.Brand>
-            </Container>
-      </Navbar>
+const Header: React.FunctionComponent<HeaderProps> = ({ totalNotes, pinnedNotes }) => {
+  return (
+    <Navbar fixed="top" className="top-nav" variant="dark">
+      <Container>
+        <Navbar.Brand className="fw-bold">My Smart Notes</Navbar.Brand>
+        <span className="header-stat">
+          {totalNotes} notes • {pinnedNotes} pinned
+        </span>
+      </Container>
+    </Navbar>
   );
 };
 
